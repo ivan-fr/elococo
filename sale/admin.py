5557fe63b6e97249810f9efb9604f1132ca578b0
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2021-06-08 18:31:57 +0200

    admin sale for ordered

--- sale/admin.py
+++ sale/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from sale.models import Ordered
+from catalogue.bdd_calculations import BACK_TWO_PLACES
+
+def price_exact_ttc(obj):
+    return "{:.2f} euros".format(obj.price_exact_ttc_with_quantity_sum * BACK_TWO_PLACES)
+
+
+def price_exact_ht(obj):
+    return "{:.2f} euros".format(obj.price_exact_ht_with_quantity_sum * BACK_TWO_PLACES)
 
 @admin.register(Ordered)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('order_number', "createdAt", "endOfLife", "payment_status", 'email', 'phone', "products", "price_exact_ht_with_quantity_sum",
-                    "price_exact_ttc_with_quantity_sum")
-    filter_horizontal = ('payment_status',)
-    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('order_number', "createdAt", "endOfLife", "payment_status", 'email', 'phone', price_exact_ht,
+                    price_exact_ttc)
+    list_filter = ("payment_status",)
+
+    change_form_template = "sale/admin/order_change_form.html"
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
     def get_queryset(self, request):
         queryset = super(ProductAdmin, self).get_queryset(request)
