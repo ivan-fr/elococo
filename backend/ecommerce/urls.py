3b163686d631cec9dd4e442989c1b4deb1bb7c43
ivanbesevic <ivan.besevic_fr@yahoo.com>
2024-06-20 16:18:17 +0200

    improve

--- backend/ecommerce/urls.py
+++ backend/ecommerce/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 import catalogue.views
 from catalogue.urls import router as catalogue_router
