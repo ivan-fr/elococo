0fbfafad868ca73a5ed615e6fb783f6086fca366
ivanbesevic <ivan.besevic_fr@yahoo.com>
2024-10-12 16:14:46 +0200

    improve

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -3,21 +3,37 @@ from django.contrib.auth.models import Group
 
 from ecommerce.admin_site import admin_site
 from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
 
 class UserAdmin(BaseUserAdmin):
     ordering = ('email',)
     list_display = ('email', 'is_staff')
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        ('Important dates', {'fields': ('last_login',)}),
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "usable_password", "password1", "password2"),
+            },
+        ),
     )
     search_fields = ('email',)
     filter_horizontal = ()
