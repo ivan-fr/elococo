b7029f0f5ef8d7019cd9902e8e744ed0dfa18ddf
ivanbesevic <ivan.besevic_fr@yahoo.com>
2024-10-24 13:04:58 +0200

    improve

--- backend/ecommerce/admin_site.py
+++ backend/ecommerce/admin_site.py
@@ -1,7 +1,8 @@
 from django.contrib.admin import AdminSite
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin
-
+from django.contrib.sites.models import Site
+from django.contrib.sites.admin import SiteAdmin
 
 class MyAdminSite(AdminSite):
     site_header = "Ecommerce Admin"
@@ -11,3 +12,4 @@ class MyAdminSite(AdminSite):
 
 admin_site = MyAdminSite()
 admin_site.register(FlatPage, FlatPageAdmin)
+admin_site.register(Site, SiteAdmin)
